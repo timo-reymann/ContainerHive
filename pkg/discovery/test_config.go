35cb076524f99b9b2e9286143637c2db78dca233
Timo Reymann <mail@timo-reymann.de>
2026-02-08 19:02:37 +0100

    docs: Add code coverage badge

--- pkg/discovery/test_config.go
+++ pkg/discovery/test_config.go
@@ -1,24 +1,11 @@
 package discovery
 
 import (
-	"errors"
-	"os"
-	"path/filepath"
-
 	"github.com/timo-reymann/ContainerHive/internal/file_resolver"
 )
 
 var testConfigFileNames = file_resolver.GetFileCandidates("test", "yml", "yaml")
 
 func getTestConfigFilePath(root string) (string, error) {
-	for _, name := range testConfigFileNames {
-		_, err := os.Stat(filepath.Join(root, name))
-		if err != nil && !os.IsNotExist(err) {
-			return "", errors.Join(errors.New("failed to stat test config file path "+name), err)
-		}
-		if err == nil {
-			return filepath.Join(root, name), nil
-		}
-	}
-	return "", nil
+	return file_resolver.ResolveFirstExistingFile(root, testConfigFileNames...)
 }
